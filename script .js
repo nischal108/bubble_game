a7f17a14dac13c42c902ef3de43b9e528515573a
Nischal Bhattarai <nishchalb21@gmail.com>
2023-08-13 20:30:07 +0545

    completed

--- script .js
+++ script .js
@@ -1,4 +1,7 @@
 
+var hit;
+
+var score = document.getElementById("score").innerHTML;
 // functon to generate coins and random numbers inside them
 function generatecoin() {
     var rem = " ";
@@ -8,7 +11,7 @@ for (let i = 0; i < 189; i++) {
 }
 
 document.getElementById("pbtm").innerHTML = rem;
-console.log(random);
+// console.log(random);
 }
 
 //function for timer
@@ -17,13 +20,49 @@ function timer() {
     var timerint = setInterval(function() {
         if (time > 0) { 
             time--;
-            console.log(document.querySelector("#timerva").innerHTML = time);
+            document.querySelector("#timerva").innerHTML = time;
         }
         
         else{
             clearInterval(timerint);
+            document.getElementById("pbtm").innerHTML =`<div id="over"><h1>&#127881; Game Over &#127881;</h1> <p> Refresh the page to play the game again </p></div>`;
         }
     }, 1000);
     }
+
+// generate random number in hit
+function hitgenerator() {
+    hit = Math.floor(Math.random()*10);
+    document.getElementById("hitnumber").innerHTML=hit;
+}
+
+
+//function to increase the score by 10 , that will be called when user hits the hit
+
+function increasescore(){
+    score = Number(score);
+    score +=10;
+    document.getElementById("score").innerHTML = score;
+}
+
+
+//listening to the user click on the button 
+//checking if the clicked bubble matches the value in hit
+
+document.getElementById("pbtm").addEventListener("click",function(details){
+    var userClick =Number((details.target.innerHTML));
+    if(userClick===hit){
+        increasescore();
+        generatecoin();
+        hitgenerator();
+   }
+})
+
+
+
+
+
+
 generatecoin();
 timer();
+hitgenerator();
